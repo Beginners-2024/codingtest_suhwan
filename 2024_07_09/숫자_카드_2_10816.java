e66c300f8451039e099801e5b871edd9c0932024
parksooo <popomarin28@gmail.com>
2024-07-09 15:57:05 +0900

    [feat] : solve runtime

--- 2024_07_09/숫자_카드_2_10816.java
+++ 2024_07_09/숫자_카드_2_10816.java
@@ -32,8 +32,37 @@ public class 숫자_카드_2_10816 {
 
         StringBuilder sb = new StringBuilder();
 
+        /**
+         * 처음에는 n 의 index를 찾아서 while문을 돌면서 첫번째 n의 시작 인덱스와 끝 인덱스를 구하는 방식으로 풀이
+         * 하지만, 시간초과가 나왔다. ex) [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1.........,1,1,1,1,1] 중간 값이 1이면 0, 배열 길이까지
+         * while을 순회해야하기 때문....
+         *
+         * 해결책 :
+         * 이진 탐색을 하면서, 시작 인덱스와 끝 인덱스를 각각 구하는 method를 만들기로 함
+         * 같은 이진 탐색이지만, 시작과 끝을 판단할 수 있는 방법을 갈구하게 됌;;
+         * 해답은 arr[mid] 값에 대한 판단을 다르게 하는 것이다.
+         * lower index를 찾을때는, key <= arr[mid] 라면 end를 mid 값으로 계속 옮긴다.
+         *  작거나 같을 경우 계속해서 찾는 것 : [1, 3, 3, 3, 3, 10]
+         *  1번째 [1, 3, 3, 3, 3, 10] -> start = 0, end = 6, mid = 3;
+         * |               *       |
+         *  2번째 [1, 3, 3, 3, 3, 10] -> start = 0, end = 3, mid = 1;
+         * |         *             |
+         *  3번째 [1, 3, 3, 3, 3, 10] -> start = 0, end = 1, mid = 1;
+         * |     *                 |
+         *  4번쩨 [1, 3, 3, 3, 3, 10] -> start = 0, end = 1, mid = 0;
+         * |      *                | -> start = 1, end = 1, mid = 1;
+         * Return start = 1; arr[start] = 3;
+         *
+         * upper index를 찾을 때는, key < arr[mid] 라면 end를 mid 값으로 계속 옮긴다.
+         *  1번째 [1, 3, 3, 3, 3, 10] -> start = 0, end = 6, mid = 3;
+         * |               *       |
+         *  2번째 [1, 3, 3, 3, 3, 10] -> start = 4, end = 3, mid = 3;
+         * |                  *    |
+         * Return start = 4; arr[start] = 3;
+         */
         for (int i = 0; i < M; i++) {
-            sb.append(getCount(integerArr[i])).append(' ');
+            int cnt = upperBoundary(integerArr[i]) - lowerBoundary(integerArr[i]);
+            sb.append(cnt).append(' ');
         }
 
         bw.write(sb.toString());
@@ -41,50 +70,36 @@ public class 숫자_카드_2_10816 {
         bf.close();
     }
 
-    private static int getCount(int n) {
+    private static int lowerBoundary(int n) {
         int start = 0;
-        int end = N - 1;
-        int key = -1;
-        int mid;
-        while (start <= end) {
-            mid = (start + end) / 2;
-            if (n == own[mid]) {
-                key = mid;
-                break;
-            } else if (n < own[mid]) {
-                end = mid - 1;
+        int end = N;
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (n <= own[mid]) {
+                end = mid;
             } else {
                 start = mid + 1;
             }
         }
-        if (key == -1) {
-            return 0;
+        return start;
+    }
+
+    private static int upperBoundary(int n) {
+        int start = 0;
+        int end = N;
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+
+            if (n < own[mid]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
         }
 
-        int left;
-        int tmp = key;
-        while (tmp > 0 && own[tmp] == n) {
-            tmp--;
-        }
-        if (tmp == 0) {
-            left = 0;
-        } else {
-            left = tmp + 1;
-        }
-
-        int right;
-        tmp = key;
-        while (tmp < N && own[tmp] == n) {
-            tmp++;
-        }
-        right = tmp - 1;
-
-        return right - left + 1;
+        return start;
     }
 }
-/**
- * 10
- * 6 3 2 10 10 10 -10 -10 7 3
- * 8
- * 10 9 -5 2 3 4 5 -10
- */
